000248a5c3736eea5c7c83cc08b74780c7460c6e
LBielskus <liudbiel@gmail.com>
2024-04-15 13:18:09 +0300

    commit

--- components/IconCards.js
+++ components/IconCards.js
@@ -33,8 +33,8 @@ const ServicesCards = () => {
 
 const Card = ({ title, icon }) => {
   return (
-    <div className='bg-white  rounded-2xl p-6  h-200 flex flex-col items-center justify-center shadow-2xl w-[200px] md:w-[200px] lg:w-[250px] xl:w-[250px] mx-auto'>
-      {icon}
+    <div className='bg-white rounded-2xl p-6 h-200 flex flex-col items-center justify-center shadow-2xl w-[200px] md:w-[200px] lg:w-[250px] xl:w-[250px] mx-auto'>
+      <div className='icon-container text-6xl text-button'>{icon}</div>
 
       <button className='h-15 mt-11 bg-button text-white px-4 py-2 rounded-md hover:bg-red-700 transition-colors'>
         {title}
